48a2dd8d2f980c47b8c248fdc2173c913dbe8dd4
alMukaafih <almukaafih@gmail.com>
2024-06-19 03:11:46 +0100

    UPDATE: Fix js workflow 5

--- naqsh-js/index.js
+++ naqsh-js/index.js
@@ -32,10 +32,10 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'naqsh.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'naqsh-js.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.android-arm64.node')
+            nativeBinding = require('./naqsh-js.android-arm64.node')
           } else {
             nativeBinding = require('naqsh-android-arm64')
           }
@@ -44,10 +44,10 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'naqsh.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'naqsh-js.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.android-arm-eabi.node')
+            nativeBinding = require('./naqsh-js.android-arm-eabi.node')
           } else {
             nativeBinding = require('naqsh-android-arm-eabi')
           }
@@ -63,11 +63,11 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'naqsh.win32-x64-msvc.node')
+          join(__dirname, 'naqsh-js.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.win32-x64-msvc.node')
+            nativeBinding = require('./naqsh-js.win32-x64-msvc.node')
           } else {
             nativeBinding = require('naqsh-win32-x64-msvc')
           }
@@ -77,11 +77,11 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'naqsh.win32-ia32-msvc.node')
+          join(__dirname, 'naqsh-js.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.win32-ia32-msvc.node')
+            nativeBinding = require('./naqsh-js.win32-ia32-msvc.node')
           } else {
             nativeBinding = require('naqsh-win32-ia32-msvc')
           }
@@ -91,11 +91,11 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'naqsh.win32-arm64-msvc.node')
+          join(__dirname, 'naqsh-js.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.win32-arm64-msvc.node')
+            nativeBinding = require('./naqsh-js.win32-arm64-msvc.node')
           } else {
             nativeBinding = require('naqsh-win32-arm64-msvc')
           }
@@ -108,10 +108,10 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'naqsh.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'naqsh-js.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./naqsh.darwin-universal.node')
+        nativeBinding = require('./naqsh-js.darwin-universal.node')
       } else {
         nativeBinding = require('naqsh-darwin-universal')
       }
@@ -119,10 +119,10 @@ switch (platform) {
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'naqsh.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'naqsh-js.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.darwin-x64.node')
+            nativeBinding = require('./naqsh-js.darwin-x64.node')
           } else {
             nativeBinding = require('naqsh-darwin-x64')
           }
@@ -132,11 +132,11 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'naqsh.darwin-arm64.node')
+          join(__dirname, 'naqsh-js.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.darwin-arm64.node')
+            nativeBinding = require('./naqsh-js.darwin-arm64.node')
           } else {
             nativeBinding = require('naqsh-darwin-arm64')
           }
@@ -152,10 +152,10 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'naqsh.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'naqsh-js.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./naqsh.freebsd-x64.node')
+        nativeBinding = require('./naqsh-js.freebsd-x64.node')
       } else {
         nativeBinding = require('naqsh-freebsd-x64')
       }
@@ -168,11 +168,11 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-x64-musl.node')
+            join(__dirname, 'naqsh-js.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-x64-musl.node')
+              nativeBinding = require('./naqsh-js.linux-x64-musl.node')
             } else {
               nativeBinding = require('naqsh-linux-x64-musl')
             }
@@ -181,11 +181,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-x64-gnu.node')
+            join(__dirname, 'naqsh-js.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-x64-gnu.node')
+              nativeBinding = require('./naqsh-js.linux-x64-gnu.node')
             } else {
               nativeBinding = require('naqsh-linux-x64-gnu')
             }
@@ -197,11 +197,11 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-arm64-musl.node')
+            join(__dirname, 'naqsh-js.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-arm64-musl.node')
+              nativeBinding = require('./naqsh-js.linux-arm64-musl.node')
             } else {
               nativeBinding = require('naqsh-linux-arm64-musl')
             }
@@ -210,11 +210,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-arm64-gnu.node')
+            join(__dirname, 'naqsh-js.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-arm64-gnu.node')
+              nativeBinding = require('./naqsh-js.linux-arm64-gnu.node')
             } else {
               nativeBinding = require('naqsh-linux-arm64-gnu')
             }
@@ -226,11 +226,11 @@ switch (platform) {
       case 'arm':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-arm-musleabihf.node')
+            join(__dirname, 'naqsh-js.linux-arm-musleabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-arm-musleabihf.node')
+              nativeBinding = require('./naqsh-js.linux-arm-musleabihf.node')
             } else {
               nativeBinding = require('naqsh-linux-arm-musleabihf')
             }
@@ -239,11 +239,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-arm-gnueabihf.node')
+            join(__dirname, 'naqsh-js.linux-arm-gnueabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-arm-gnueabihf.node')
+              nativeBinding = require('./naqsh-js.linux-arm-gnueabihf.node')
             } else {
               nativeBinding = require('naqsh-linux-arm-gnueabihf')
             }
@@ -255,11 +255,11 @@ switch (platform) {
       case 'riscv64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-riscv64-musl.node')
+            join(__dirname, 'naqsh-js.linux-riscv64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-riscv64-musl.node')
+              nativeBinding = require('./naqsh-js.linux-riscv64-musl.node')
             } else {
               nativeBinding = require('naqsh-linux-riscv64-musl')
             }
@@ -268,11 +268,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'naqsh.linux-riscv64-gnu.node')
+            join(__dirname, 'naqsh-js.linux-riscv64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./naqsh.linux-riscv64-gnu.node')
+              nativeBinding = require('./naqsh-js.linux-riscv64-gnu.node')
             } else {
               nativeBinding = require('naqsh-linux-riscv64-gnu')
             }
@@ -283,11 +283,11 @@ switch (platform) {
         break
       case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, 'naqsh.linux-s390x-gnu.node')
+          join(__dirname, 'naqsh-js.linux-s390x-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./naqsh.linux-s390x-gnu.node')
+            nativeBinding = require('./naqsh-js.linux-s390x-gnu.node')
           } else {
             nativeBinding = require('naqsh-linux-s390x-gnu')
           }
